7227f1307bf8a504d8036024da4256e79d405a5b
Oğulcan <ogulcan06-07@hotmail.com>
2020-11-03 05:32:24 +0300

    New Push
    Changed theme, script.
    Added bootstrap cdn, ajax.

--- script.js
+++ script.js
@@ -1,14 +1,49 @@
-$.ajax({
-    url: "https://geolocation-db.com/jsonp",
-    jsonpCallback: "callback",
-    dataType: "jsonp",
-    success: function(location) {
-      $('#country').html(location.country_name);
-      $('#state').html(location.state);
-      $('#city').html(location.city);
-      $('#latitude').html(location.latitude);
-      $('#longitude').html(location.longitude);
-      $('#ip').html(location.IPv4);
-      $('#postal').html(location.postal);
+var x = document.getElementById("coordinate");
+var y = document.getElementById("json");
+var z = document.getElementById("city");
+var a = document.getElementById("country");
+var b = document.getElementById("district");
+var c = document.getElementById("");
+function getLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition);
+  } else {
+    x.innerHTML = "Geolocation is not supported by this browser.";
+  }
+}
+
+function showPosition(position) {
+  x.innerHTML =
+    "Latitude: " +
+    position.coords.latitude +
+    "<br>Longitude: " +
+    position.coords.longitude;
+
+  //Create query for the API.
+  var latitude = "latitude=" + position.coords.latitude;
+  var longitude = "&longitude=" + position.coords.longitude;
+  var query = latitude + longitude + "&localityLanguage=en";
+
+  const Http = new XMLHttpRequest();
+
+  var bigdatacloud_api =
+    "https://api.bigdatacloud.net/data/reverse-geocode-client?";
+
+  bigdatacloud_api += query;
+
+  Http.open("GET", bigdatacloud_api);
+  Http.send();
+
+  Http.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      var myObj = JSON.parse(this.responseText);
+      console.log(myObj);
+      y.innerHTML += "" + myObj.postcode;
+      b.innerHTML += "" + myObj.locality;
+      a.innerHTML += "" + myObj.countryName;
+      z.innerHTML += "" + myObj.principalSubdivision;
     }
-  });
+  };
+}
+
+getLocation();
